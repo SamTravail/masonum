6375ed268afd65cc838e8c241c6277a8731018c7
SabyDev <83217408+SabyDev@users.noreply.github.com>
2022-08-29 17:38:03 +0200

    changement de footer, et essaie menu deroulant dans le header

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -3,16 +3,27 @@
 namespace App\Controller;
 
 use App\Repository\ArticlesRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class ArticlesController extends AbstractController
 {
     #[Route('/articles', name: 'app_articles')]
-    public function index(ArticlesRepository $repository): Response
+    public function index(ArticlesRepository $repository, PaginatorInterface $paginator,
+    Request $request
+): Response
     {
-        $articles = $repository->findAll();
+        $articles = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
         
         return $this->render('pages/articles/index.html.twig', [
            'articles'=>$articles 
