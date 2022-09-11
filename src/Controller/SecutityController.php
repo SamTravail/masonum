71d4df25ad6d7906426713498bbf65307a8ddd81
SabyDev <83217408+SabyDev@users.noreply.github.com>
2022-09-11 20:35:00 +0200

    message erreur pour formulaire inscription

--- src/Controller/SecutityController.php
+++ src/Controller/SecutityController.php
@@ -5,16 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecutityController extends AbstractController
 {
     #[Route('/connexion', name: 'app_login', methods: ['GET', 'POST'])]
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         return $this->render('pages/secutity/login.html.twig', [
-            'controller_name' => 'SecutityController',
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error' => $authenticationUtils->getLastAuthenticationError()
         ]);
     }
+
     #[Route('/deconnexion', name: 'app_logout')]
     public function logout()
     {
