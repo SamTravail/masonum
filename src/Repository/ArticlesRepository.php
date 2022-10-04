89a304e121e7ef953cc7e8eb727a2ebb6c8a211c
SabyDev <83217408+SabyDev@users.noreply.github.com>
2022-10-04 18:37:45 +0200

    affichage des articles de l'utilisateur et public sur une  pages article public. affichage des 3 derniers articles sur la paage d'accueil

--- src/Repository/ArticlesRepository.php
+++ src/Repository/ArticlesRepository.php
@@ -20,7 +20,22 @@ class ArticlesRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Articles::class);
     }
-
+    public function findPublicArticle(?int $nbArticles): array
+    {
+        $queryBuilder = $this->createQueryBuilder('r')
+            //   language sql true = 1 false =0
+            ->where('r.isPublic = 1')
+            // classer par ordre decroissant date
+            ->orderBy('r.createdAt', 'DESC');
+        // mettre un nombre max d'articles si le nbArticle est différent de 0
+        if ($nbArticles !== 0 || $nbArticles !== null) {
+            $queryBuilder->setMaxResults($nbArticles);
+        }
+        // recuperé la query
+        return $queryBuilder->getQuery()
+            // récupérer de la query le resultat
+            ->getResult();
+    }
     public function add(Articles $entity, bool $flush = false): void
     {
         $this->getEntityManager()->persist($entity);
@@ -39,28 +54,28 @@ class ArticlesRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Articles[] Returns an array of Articles objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('a.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Articles[] Returns an array of Articles objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('a')
+    //            ->andWhere('a.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('a.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?Articles
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Articles
+    //    {
+    //        return $this->createQueryBuilder('a')
+    //            ->andWhere('a.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
